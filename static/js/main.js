8a45cc0ae96f09ed7bdac06fe4016507375dfeac
Leonard Techel <mail@telelo.de>
2012-11-07 00:19:05 +0100

    websocket closed overlay added; typing delay on sending added (reduces server load)

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,14 @@ var NextPad = (function (jQuery) {
 
 	var $ = jQuery.sub();
 
-	function NextPad(padID) {
-		this.config = false;
+	function NextPad(config) {
+		$.editable = $(config.editable);
+
+		this.config = config;
+		this.lastContent = "";
+		this.thisContent = "";
+		this.dmp = new diff_match_patch();
+		this.ws = new WebSocket(this.config.ws);
 	}
 	(function (fn) {
 		fn.run = function() {
@@ -14,50 +20,68 @@ var NextPad = (function (jQuery) {
 			}
 
 			var instance = this,
-				editable = $(this.config.editable),
-				lastContent,
-				thisContent,
 				patch,
 				gotJSON = {},
-				dmp = new diff_match_patch();
+				whileTyping;
 
-			// open the websocket
-			var ws = new WebSocket(instance.config.ws);
-			ws.onopen = function() {
+			// configure the websocket
+			instance.ws.onopen = function() {
 				console.log("WebSocket opened.");
-				ws.send(JSON.stringify({"padID": instance.config.padID}));
+				instance.ws.send(JSON.stringify({"padID": instance.config.padID}));
 			};
-			ws.onmessage = function(evt) {
+			instance.ws.onmessage = function(evt) {
 				gotJSON = $.parseJSON(evt.data);
 				if("text" in gotJSON) {
-					editable.html(gotJSON["text"]);
-					lastContent = gotJSON["text"];
+					$.editable.html(gotJSON["text"]);
+					this.lastContent = gotJSON["text"];
+
+					$('#loading').hide();
 				} else if("patch" in gotJSON) {
-					console.log(gotJSON);
-					lastContent = editable.html();
-					thisContent = dmp.patch_apply(dmp.patch_fromText(gotJSON["patch"]), lastContent)[0];
-					editable.html(thisContent);
+					instance.applyPatch(gotJSON["patch"]);
 				}
+			};
+			instance.ws.onclose = function() {
+				$('#loading').show();
 			};
 
 			// make the content object editable and set the 
-			editable.attr("contenteditable", true)
-				.keyup(function() {
-					console.log("Keyup!");
-					thisContent = editable.html();
+			$.editable.attr("contenteditable", true)
+				.typing({
+					start: function() {
+						// start an interval which is continously sending a patch
+						whileTyping = setInterval(function() {
+							instance.makeSendPatch();
+						}, 700);
+					},
+					stop: function() {
+						// stop the continous interval
+						clearInterval(whileTyping);						
 
-					// generate the diff if something changed
-					if(lastContent != thisContent) {
-						patch = dmp.patch_toText(dmp.patch_make(lastContent, thisContent));
-
-						// send it to the server
-						ws.send(JSON.stringify({"patch": patch}));
-
-						// save it as last
-						lastContent = thisContent;
-					}
+						instance.makeSendPatch();
+					},
+					delay: 300
 				});
+		};
+		fn.applyPatch = function(patch) {
+			this.lastContent = $.editable.html();
+			this.thisContent = this.dmp.patch_apply(this.dmp.patch_fromText(patch), this.lastContent)[0];
+			$.editable.html(this.thisContent);
+		};
+		fn.makeSendPatch = function() {
+			var instance = this,
+				patch;
 
+			// get the new content
+			instance.thisContent = $.editable.html();
+
+			// generate the diff if something changed
+			if(instance.lastContent != instance.thisContent) {
+				patch = instance.dmp.patch_toText(instance.dmp.patch_make(instance.lastContent, instance.thisContent));
+				// send it to the server
+				instance.ws.send(JSON.stringify({"patch": patch}));
+				// save it as last
+				instance.lastContent = instance.thisContent;
+			}
 		};
 	}(NextPad.prototype));
 
